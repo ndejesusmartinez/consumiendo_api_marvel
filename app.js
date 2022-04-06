5ea6fc0fbed0ae6fc490fa29724f401a6588fc56
ndejesusmartinez <nmartinezc97@hotmail.com>
2022-04-06 13:57:58 -0500

    @Naren

--- app.js
+++ app.js
@@ -10,25 +10,20 @@ document.querySelector('#Listar_series').addEventListener('click',function(){
   obtenerSeries();
 });
 
-function obtenerHeroes(){
-const url = `https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=1b4a71597fe79145195683ef11f4635b&hash=55f6472b12db4ff84b98cf97e899c1a0`;
-fetch(url)
-.then(res=>res.json())
-.then((json)=>{
-  let items = json.data.results;
-  console.log(items)
-  let ts = '1'
-  let apikey = '1b4a71597fe79145195683ef11f4635b'
-  let hash = '55f6472b12db4ff84b98cf97e899c1a0'
-  const HTMLResponse = document.querySelector("#resultado");
-  const tpl = items.map((items)=> `<div class="col-md-2"> <h3>${items.name}</h3>
-                                   <a href="${items.urls[1].url}"> <img src="${items.thumbnail.path}.${items.thumbnail.extension}" alt="${items.name}" class="img-thumbnail"><br> </a> <br>
+async function obtenerHeroes(){
+const respuesta = await fetch(`https://gateway.marvel.com:443/v1/public/characters?limit=20&ts=1&apikey=1b4a71597fe79145195683ef11f4635b&hash=55f6472b12db4ff84b98cf97e899c1a0`);
+console.log(respuesta)
 
-
-
-  </div>`);
-  HTMLResponse.innerHTML = tpl;
+const datos = await respuesta.json();
+let personajes = datos.data.results;
+console.log(personajes)
+let heroes='';
+personajes.forEach(persona=>{
+  heroes+= `<div class=""> <h3>${persona.name}</h3>
+  <a href="${persona.urls[1].url}"> <img src="${persona.thumbnail.path}.${persona.thumbnail.extension}" alt="${persona.name}" class="img-thumbnail"><br> </a> <br>
+  </div>`
 })
+document.getElementById('resultado').innerHTML=heroes;
 };
 
 function obtenerComics(){
