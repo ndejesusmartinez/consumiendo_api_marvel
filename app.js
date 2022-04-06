930084907e56b93419dff5a8125328cb620f9991
ndejesusmartinez <nmartinezc97@hotmail.com>
2022-04-06 14:20:19 -0500

    @Naren

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ document.querySelector('#Listar_series').addEventListener('click',function(){
 
 async function obtenerHeroes(){
 const respuesta = await fetch(`https://gateway.marvel.com:443/v1/public/characters?limit=20&ts=1&apikey=1b4a71597fe79145195683ef11f4635b&hash=55f6472b12db4ff84b98cf97e899c1a0`);
-console.log(respuesta)
+//console.log(respuesta)
 
 const datos = await respuesta.json();
 let personajes = datos.data.results;
-console.log(personajes)
+//console.log(personajes)
 let heroes='';
 personajes.forEach(persona=>{
   heroes+= `<div class=""> <h3>${persona.name}</h3>
@@ -26,32 +26,30 @@ personajes.forEach(persona=>{
 document.getElementById('resultado').innerHTML=heroes;
 };
 
-function obtenerComics(){
-  const url = `https://gateway.marvel.com:443/v1/public/comics?limit=100&ts=1&apikey=1b4a71597fe79145195683ef11f4635b&hash=55f6472b12db4ff84b98cf97e899c1a0`
-  fetch(url)
-  .then(res=>res.json())
-  .then((json)=>{
-    let items=json.data.results;
-    console.log(items)
-    const HTMLResponse = document.querySelector("#resultado");
-    const tpl = items.map((items)=> ` <div class="col-md-3"> <h3>${items.title}</h3>
-                                         <a href="${items.urls[0].url}"> <img src="${items.thumbnail.path}.${items.thumbnail.extension}" alt="${items.title}"class="img-thumbnail"></a>
-    </div>`);
-    HTMLResponse.innerHTML = tpl;
-  })
+async function obtenerComics(){
+  const respuesta = await fetch(`https://gateway.marvel.com:443/v1/public/comics?limit=100&ts=1&apikey=1b4a71597fe79145195683ef11f4635b&hash=55f6472b12db4ff84b98cf97e899c1a0`);
+    const datos = await respuesta.json();
+    let comics = datos.data.results;
+    let comicsComic ='';
+    comics.forEach(comi=>{
+      comicsComic+=`<div>
+                    <h3>${comi.title}</h3>
+                    <a href="${comi.urls[0].url}"> <img src="${comi.thumbnail.path}.${comi.thumbnail.extension}" alt="${comi.title}"class="img-thumbnail"></a>
+      </div>`
+    })
+    document.getElementById('resultado').innerHTML=comicsComic;
 };
 
-function obtenerSeries(){
-  const url = `https://gateway.marvel.com:443/v1/public/series?limit=100&ts=1&apikey=1b4a71597fe79145195683ef11f4635b&hash=55f6472b12db4ff84b98cf97e899c1a0`
-  fetch(url)
-  .then(res=>res.json())
-  .then((json)=>{
-    let items=json.data.results;
-    console.log(items)
-    const HTMLResponse = document.querySelector("#resultado");
-    const tpl = items.map((items)=> `<div class="col-md-3"> <h3>${items.title}</h3
-                                         <a href="${items.urls[0].url}"> <img src="${items.thumbnail.path}.${items.thumbnail.extension}" alt="${items.title}"class="img-thumbnail"></a>
-    </div>`);
-    HTMLResponse.innerHTML = tpl;
+async function obtenerSeries(){
+  const respuesta = await fetch(`https://gateway.marvel.com:443/v1/public/series?limit=100&ts=1&apikey=1b4a71597fe79145195683ef11f4635b&hash=55f6472b12db4ff84b98cf97e899c1a0`);
+  const datos = await respuesta.json();
+  let series=datos.data.results;
+  let totalSeries = '';
+  series.forEach(seri=>{
+    totalSeries+=`<div>
+                  <h3>${seri.title}</h3>
+                  <a href="${seri.urls[0].url}"> <img src="${seri.thumbnail.path}.${seri.thumbnail.extension}" alt="${seri.title}"class="img-thumbnail"></a>
+    </div>`
   })
+  document.getElementById('resultado').innerHTML=totalSeries;
 };
